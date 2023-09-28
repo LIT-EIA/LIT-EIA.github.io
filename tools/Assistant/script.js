752ce32db6d987d5265f52cbd5afcfa4c835b181
Ahmedamd <ahmed.a.md@outlook.com>
2023-09-28 16:10:32 -0400

    version - Sept 28

--- tools/Assistant/script.js
+++ tools/Assistant/script.js
@@ -79,11 +79,7 @@ function sendMessage(){
 
 			let a =  keywordsArray[i].join(' ');;
 
-			console.log("Keywords Array: ", a);
-			
-			
-			// keywords = popaulateKeywords;
-			// console.log("line send message: ",keywords);
+		
 
 			if (isMatchKeywords(messageParts, a)){
 				
@@ -100,53 +96,59 @@ function sendMessage(){
 			}
 		}
 
-		if(textboxMessage.value === "hello" || "hi" || "hi there" || " hey" ||"greetings"|| " salutations" ||"howdy"|| "what's up" ||  "yo" || "hiya" ||"how are you" ||"how's it going"
-		|| "hello" || "hi" || "hi there" || " hey" ||"greetings"|| " salutations" ||"howdy"|| "what's up" ||  "yo" || "hiya" ||"how are you" ||"how's it going" ||
-		"Allô" ||  "allo" || "salut" || "bonjour" || "bonsoir" || "coucou" || "salutations" ||"ça va" || "ca va" || "hey" || "yo"
-		){
-			if(isEnglish){
-				message.innerHTML = `
-						<div class ="black">
-							<p class= "black">Welcome to my page! </p> <br>
-
-							<p> I was given a predetermined list of frequently asked questions and answers to help you with anything Saba-related. </p> <br>
-
-
-							<p>To begin, you can filter my questions in three ways:</p> <br>
-							<ol>
-								<li>To find all predetermined questions, select the View all questions button, located directly in the “Suggested Questions” box on the right. </li> <br>
-								<li>To filter the questions by keyword, enter one or several keywords or an enquiry in the message field below.  </li> <br>
-								<li>To filter the questions by role, select the role related to your question directly in this bubble. </li> <br>
-							</ol>
-						</div>`
-						
-				
-
-				
-			assistantTypes(message, true);
-			}else{
-				message.innerHTML = 
-				`
-				<div class ="black">
-					<p class="black">Bienvenue sur ma page!  </p> <br>
-
-					<p> I was given a predetermined list of frequently asked questions and answers to help you with anything Saba-related. </p> <br>
-
-
-					<p>Pour commencer, vous pouvez filtrer mes questions de trois façons:  </p> <br>
-					<ol>
-						<li> Pour trouver toutes les questions prédéterminées, sélectionnez le bouton Afficher toutes les questions, situé dans la boîte « Questions suggérées » à droite.  </li> <br>
-						<li>Pour filtrer les questions par mot-clé, saisissez un ou plusieurs mots-clés ou une question dans le champ de message ci-dessous.   </li> <br>
-						<li>Pour filtrer les questions par rôle, sélectionnez le rôle lié à votre question directement dans cette bulle.  </li> <br>
-					</ol>;
-				</div>`
-				
-
-
-				
-			assistantTypes(message, true);
+		if(!foundKeyword){
+			if(textboxMessage.value === "hello" || "hi" || "hi there" || " hey" ||"greetings"|| " salutations" ||"howdy"|| "what's up" ||  "yo" || "hiya" ||"how are you" ||"how's it going"
+			|| "hello" || "hi" || "hi there" || " hey" ||"greetings"|| " salutations" ||"howdy"|| "what's up" ||  "yo" || "hiya" ||"how are you" ||"how's it going" ||
+			"Allô" ||  "allo" || "salut" || "bonjour" || "bonsoir" || "coucou" || "salutations" ||"ça va" || "ca va" || "hey" || "yo"
+			){
+	
+				console.log("here");
+				if(isEnglish){
+					message.innerHTML = `
+							<div class ="black">
+								<p class= "black">Welcome to my page! </p> <br>
+	
+								<p> I was given a predetermined list of frequently asked questions and answers to help you with anything Saba-related. </p> <br>
+	
+	
+								<p>To begin, you can filter my questions in three ways:</p> <br>
+								<ol>
+									<li>To find all predetermined questions, select the View all questions button, located directly in the “Suggested Questions” box on the right. </li> <br>
+									<li>To filter the questions by keyword, enter one or several keywords or an enquiry in the message field below.  </li> <br>
+									<li>To filter the questions by role, select the role related to your question directly in this bubble. </li> <br>
+								</ol>
+							</div>`
+							
+					
+	
+					
+				assistantTypes(message, true);
+				}else{
+					message.innerHTML = 
+					`
+					<div class ="black">
+						<p class="black">Bienvenue sur ma page!  </p> <br>
+	
+						<p> I was given a predetermined list of frequently asked questions and answers to help you with anything Saba-related. </p> <br>
+	
+	
+						<p>Pour commencer, vous pouvez filtrer mes questions de trois façons:  </p> <br>
+						<ol>
+							<li> Pour trouver toutes les questions prédéterminées, sélectionnez le bouton Afficher toutes les questions, situé dans la boîte « Questions suggérées » à droite.  </li> <br>
+							<li>Pour filtrer les questions par mot-clé, saisissez un ou plusieurs mots-clés ou une question dans le champ de message ci-dessous.   </li> <br>
+							<li>Pour filtrer les questions par rôle, sélectionnez le rôle lié à votre question directement dans cette bulle.  </li> <br>
+						</ol>;
+					</div>`
+					
+	
+	
+					
+				assistantTypes(message, true);
+				}
 			}
 		}
+
+		
 		
 		if (foundKeyword){
 			var message = document.createElement("p");
